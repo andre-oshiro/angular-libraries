3cbbf1b91278d1c98fad209bb6cc8271616e3bb4
oosha <oosha.github@gmail.com>
2021-04-12 21:40:52 +0900

    feat(card-list): Update for lazyload version rc.10

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DUMMY_DATA } from './card-list-dummy-data';
 import { LazyLoadOptions } from 'lazy-load-image';
+import { DummyData } from './card-list-dummy-data';
 
 @Component({
   selector: 'app-card-list',
@@ -8,9 +9,13 @@ import { LazyLoadOptions } from 'lazy-load-image';
   styleUrls: ['./card-list.component.scss']
 })
 export class CardListComponent {
-  public readonly DUMMY_DATA = DUMMY_DATA;
-
+  public readonly DUMMY_DATA: DummyData[] = [];
   public readonly lazyLoadOptions: LazyLoadOptions = {
     threshold: [0.5]
   };
+  constructor() {
+    for (let i = 0; i < 50; i++) {
+      this.DUMMY_DATA.push(DUMMY_DATA);
+    }
+  }
 }
